2b2e008bcb2e7ea86c92b31283802028526e8820
nayna <nainavaghasiya9@gmail.com>
2019-06-20 19:02:50 -0400

    Mobile Class Added

---
+++ C0753560_MID_TERM_MAD3004/C0753560_MID_TERM_MAD3004/Mobile.swift
@@ -0,0 +1,14 @@
+//
+//  Mobile.swift
+//  C0753560_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Mobile : Bill{
+    
+    
+    
+}
