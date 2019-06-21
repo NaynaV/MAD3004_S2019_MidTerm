72c713865c6c2f58ba720de38315bd3782e2632a
nayna <nainavaghasiya9@gmail.com>
2019-06-20 20:01:07 -0400

    code added

--- C0753560_MID_TERM_MAD3004/C0753560_MID_TERM_MAD3004/Extensions/currencyExtension.swift
+++ C0753560_MID_TERM_MAD3004/C0753560_MID_TERM_MAD3004/Extensions/currencyExtension.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  currencyExtension.swift
 //  C0753560_MID_TERM_MAD3004
 //
 //  Created by MacStudent on 2019-06-20.
@@ -8,5 +8,10 @@
 
 import Foundation
 
-
-var I1 = Internet(bilId: 1, bildate: Date, bilType: "Internet", totalBill: 56.00, providername: <#T##String#>, dataused: <#T##String#>)
+extension Float
+{
+    public func currency() -> String
+    {
+        return String.init(format: "$%0.2f", self)
+    }
+}
